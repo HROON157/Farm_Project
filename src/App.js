39174edff7a6b90c08318929b5a309b522cf2999
Haroon Shafiq <155438028+HROON157@users.noreply.github.com>
2024-07-12 18:46:33 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-// App.js
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SideBar from './Parts/SideBar';
-import Dashboard from './Parts/Dashboard';
-import MapLayout from './Parts/Map';
-import UserProfile from './Parts/UserProfile';
-import OrderManagement from './Parts/OrderManagement';
-import ItemListing from './Parts/ItemListing';
-import StoreInfo from './Parts/StoreInfo';
+import React from 'react';
 import './App.css';
-
+import SideBar from './Parts/SideBar'
 const App = () => {
-  const [activeComponent, setActiveComponent] = useState(null);
-
   return (
-    <Router>
-      <div className="app-container d-flex">
-        <SideBar setActiveComponent={setActiveComponent} />
-        <div className="content-container">
-          <div className={`header ${activeComponent ? 'active-header' : ''}`}>
-          <Routes>
-              <Route path="/Dashboard" element={<Dashboard />} />
-              <Route path="/Map" element={<MapLayout />} />
-              <Route path="/UserProfile" element={<UserProfile />} />
-              <Route path="/OrderManagement" element={<OrderManagement />} />
-              <Route path="/ItemListing" element={<ItemListing />} />
-              <Route path="/StoreInfo" element={<StoreInfo />} />
-            </Routes>
-          </div>
-          <div className={`middle ${activeComponent ? 'active-middle' : ''}`}>
-          </div>
-          <div className={`footer ${activeComponent ? 'active-footer' : ''}`}>
-          </div>
-        </div>
-      </div>
-    </Router>
+    <>
+    <SideBar/>
+    </>
   );
-}
+};
 
 export default App;
